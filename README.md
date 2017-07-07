a8b8e8d651048ea3ecde8e09159eb0588f4161d7
jeffpan <2359501@qq.com>
2017-07-07 16:47:20 +0800

    add ferup-format.py and .gitignore file

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # NuPyTools
 python tools for Nuance
+
+## pcm2wav.py
+
+**usage:** `pcm2wav.py [input pcm] [output wav]`
+
+**description**: convert .pcm file into audio file .wav
+
+## ferup-format.py
+
+**usage:**`ferup-format.py [-h] [--version] [-c COMB] fpath out task`
+
+**description:** convert corpus from Nuance linguistic team into FERUP format. 
+
+##### Two task modes:
+
+1. ws - word segmentation format
+2. pw - prosody word format
+
+##### Two generation ways:
+
+1. non-combination [default] - generate same-name file as inputs
+2. combination [c = 1] - generate one large file
---
+++ ferup-format.py
@@ -0,0 +1,104 @@
+#-*-coding:utf-8-*-
+#!\usr\bin\env py3
+
+import os
+import sys
+import re
+import codecs
+
+class Format(object):
+  '''
+  format Shanghainese (SHC) corpus from Naunce Linguistic team
+  '''
+  def __init__(self, options, logger):
+    self.base = options['fpath']
+    self.fs = os.listdir(self.base)
+    self.logger = logger
+    self.options = options
+  
+  def Process(self):
+    if self.options['comb'] == 1:
+      self.Process_comb(self.options['out'], self.options['task'])
+    else:
+      self.Process_Nocomb(self.options['out'], self.options['task'])
+
+  def Process_comb(self, out, task):
+    try:
+      outbase = os.path.dirname(os.path.abspath(out))
+      if not os.path.exists(outbase):
+        os.makedirs(outbase)
+        self.logger.info("%s does not exists, will be created!" % outbase)
+      with codecs.open(out, 'w', 'utf-8') as fo:
+        self.fs = list(sorted(self.fs))
+        for f in self.fs:
+          fn = os.path.join(self.base, f)
+          self.logger.info("Process: %s" % f)
+          with codecs.open(fn, 'r', 'utf-8') as fi:
+            for line in fi:
+              line = re.sub('[\'\+]','',line)
+              if task == 'ws':
+                line = re.sub('[\-]',' ',line)
+              line = re.sub('\s+',' ',line)
+              fo.write(line + '\n')
+            
+    except IOError as ioerror:
+      self.logger.error(ioerror)
+    except Exception as e:
+      self.logger.error(e)
+  
+  def Process_Nocomb(self, out, task):
+    try:
+      if not os.path.exists(out):
+        os.makedirs(out)
+        self.logger.info("%s does not exists, will be created!"%out)
+      for f in self.fs:
+        fn = os.path.join(self.base, f)
+        fop = os.path.join(out, f)
+        fo = codecs.open(fop, 'w', 'utf-8')
+        with codecs.open(fn, 'r', 'utf-8') as fi:
+          self.logger.info("Process: %s" % f)
+          for line in fi:
+            line = re.sub('[\'\+]','',line)
+            if task == 'ws':
+                line = re.sub('[\-]',' ',line)
+            line = re.sub('\s+',' ',line)
+            fo.write(line + '\n')
+    except IOError as ioerror:
+      self.logger.error(ioerror)
+    except Exception as e:
+      self.logger.error(e)
+
+      
+if __name__ == '__main__':
+  import time
+  import logging
+  from argparse import ArgumentParser
+
+  parser = ArgumentParser(description='ferup-format')
+  parser.add_argument("--version", action="version", version="ferup-format 1.0")
+  parser.add_argument(type=str, action="store", dest="fpath", default="", help='input raw data')
+  parser.add_argument(type=str, action="store", dest="out", default="", help='output file(for comb mode); output dir(for non-comb mode)')
+  parser.add_argument(type=str, action="store", dest="task", default="", help='pw - prosody word; ws - word segmentation')
+  parser.add_argument("-c", "--comb", action="store", dest="comb", type=int, default=0, help='0: no combination; 1: combination.')
+
+  args = parser.parse_args()
+  options = vars(args)
+
+  logger = logging.getLogger()
+  formatter = logging.Formatter('[%(asctime)s][*%(levelname)s*][%(filename)s:%(lineno)d|%(funcName)s] - %(message)s', '%Y%m%d-%H:%M:%S')
+  file_handler = logging.FileHandler('LOG-ferup-format.txt', 'w')
+  file_handler.setFormatter(formatter)
+  logger.addHandler(file_handler)
+
+  stream_handler = logging.StreamHandler()
+  stream_handler.setFormatter(formatter)
+  logger.addHandler(stream_handler)
+  logger.setLevel(logging.INFO)
+
+  allStartTP = time.time()
+  appInst = Format(options, logger)
+  appInst.Process()
+  allEndTP = time.time()
+  logger.info("Operation Finished [Time Cost:%0.3f Seconds]" % float(allEndTP - allStartTP))
+
+
